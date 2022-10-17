35c3312e326c720d7a5fcf3c7d66e02ee20fc6c7
leonleung <mrleonleung@gmail.com>
2022-10-17 23:55:52 +0800

    feat: 4-45

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,8 +1,18 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state'
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('')
+  const dispatch = useDispatch<any>()
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    dispatch(actionCreators.searchRepositories(term))
+  }
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
