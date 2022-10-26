5a678d54d299ec3cc20f9abb7f5e64c0c0b8278f
leonleung <8357465+s010s@users.noreply.github.com>
2022-10-26 22:43:43 +0800

    feat: 7-69

---
+++ jbook/src/index.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as esbuild from 'esbuild-wasm'
+
+const root = createRoot(document.querySelector('#root')!)
+
+const App = () => {
+  const ref = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+  }
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    })
+
+    setCode(result.code)
+  }
+
+  return (
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)} value={input}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+}
+
+root.render(<App />)
